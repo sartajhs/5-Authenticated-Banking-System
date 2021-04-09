729b24d15dd72c4d469d2b2c6b09cd7d1d00aef0
Sartaj Hussain Saifi <50270209+sartajhs@users.noreply.github.com>
2021-04-09 13:01:58 +0530

    Update jquery.mousewheel.js

--- build/web/Admin/js/jquery.mousewheel.js
+++ build/web/Admin/js/jquery.mousewheel.js
@@ -1,13 +1,4 @@
-/*! Copyright (c) 2009 Brandon Aaron (http://brandonaaron.net)
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
- * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
- *
- * Version: 3.0.2
- * 
- * Requires: 1.2.2+
- */
+
 
 (function($) {
 
